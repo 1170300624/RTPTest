139cdb2927475aebc0db75d21ad58bf3607555b1
mi <827129226@qq.com>
2021-08-25 14:46:02 +0800

    2021.8.25

--- app/src/main/java/com/example/rtptest/MainActivity.java
+++ app/src/main/java/com/example/rtptest/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.rtptest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCall = findViewById(R.id.call);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    call();
+                }catch (SecurityException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            call();;
+        }
+        else {
+            Toast.makeText(this, "You Denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void call(){
+
+        try {
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:10086"));
+            startActivity(intent);
+        }catch (SecurityException e){
+            e.printStackTrace();
+        }
+
     }
 }
